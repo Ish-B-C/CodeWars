3361928d3847bf1b7f8d82e59f5d04b92306937a
ISHIMWE-Bosco <b.ishimwe3@alustudent.com>
2024-11-20 18:43:58 +0200

    Reverse String, opposite number, remove first and last string, repeat string

---
+++ 8kyu/opposite_number.js
@@ -0,0 +1,4 @@
+function opposite(number) {
+  if (number>0 || number<0 || number===0)
+    return number *-1
+}
---
+++ 8kyu/string_repeat.js
@@ -0,0 +1,8 @@
+function repeatStr (n, s) {
+  let sum= ""
+  for (let i=0;i<n; i++) {
+    sum+= s
+  }
+  
+  return sum;
+}
